2b849ebe2dc909c1f4e1130c42ac89f2b1012eb6
samson-folami <92054491+samson-folami@users.noreply.github.com>
2021-10-11 10:52:27 -0700

    Add files via upload

---
+++ First task Samson.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    // printf("Name : Samson Folami\n");
+    // printf("Email add : folami.samson@gmail.com\n");
+    // printf("Discord name : @Samson\n");
+    // printf("Biostack : Genomics\n");
+    return(0);
+}
